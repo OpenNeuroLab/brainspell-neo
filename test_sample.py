b34ab956f3b65b569573dcde2a99284e8d728a91
Neel Somani <neel@berkeley.edu>
2016-11-08 12:52:15 -0800

    updated requirements.txt for tornado testing

--- test_sample.py
+++ test_sample.py
@@ -1,5 +1,25 @@
+import tornado.web
+import pytest
+
 def func(x):
     return x + 1
 
 def test_answer():
     assert func(3) == 4
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+@pytest.fixture
+def app():
+    return application
+
+@pytest.mark.gen_test
+def test_hello_world(http_client, base_url):
+    response = yield http_client.fetch(base_url)
+    assert response.code == 200

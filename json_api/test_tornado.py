03fcc74a4271d41bdfcdefee02633ebf50e80d96
Neel Somani <neel@berkeley.edu>
2016-11-09 00:45:31 -0800

    moved CI tests

---
+++ json_api/test_tornado.py
@@ -0,0 +1,15 @@
+import tornado.web
+import os
+import pytest
+import Tornado
+
+application = Tornado.make_app()
+
+@pytest.fixture
+def app():
+    return application
+
+@pytest.mark.gen_test
+def test_front_page(http_client, base_url):
+    response = yield http_client.fetch(base_url)
+    assert response.code == 200

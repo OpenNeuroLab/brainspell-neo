d2f04df3cf3842b64c26dc260f92f909653f3871
Neel Somani <neel@berkeley.edu>
2016-11-09 00:07:53 -0800

    reorganized files; added template for front page

---
+++ json-api/Tornado.py
@@ -0,0 +1,83 @@
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+import os
+import sqlite3
+import pymysql # will be using this as a search engine for the time being
+import sqlalchemy as sql
+from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+import certifi
+import json
+import peewee
+import tornado
+
+### Relies on the MySQL Database
+"""
+Commenting out for now, just to get Heroku working (there's no database online at the moment)
+hostname = '192.168.99.100'
+username = 'root'
+password = 'beo8hkii'
+database = 'brainspell'
+myConnection = pymysql.connect(host = hostname, user = username, passwd = password, db = database)
+"""
+### End of MYSQL Setup
+
+### POSTGRES SETUP
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("templates/text.html")
+
+class AllExtras(tornado.web.RequestHandler):
+    def get(self):
+        self.render("templates/html/login.html", title = "ShowAll")
+        
+    def post(self):
+        Name = self.get_argument("text")
+        self.write(Name)
+
+
+class StoryHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("templates/text.html")
+
+    def post(self):
+        self.set_header("Content-Type", "text/plain")
+        database_dict = {}
+        text = self.get_body_argument("text")
+        cur = myConnection.cursor()
+        cur.execute("ALTER TABLE Articles ADD FULLTEXT(Title)")
+        search_string = "Select UniqueID,TIMESTAMP,Title,Authors,Abstract,Reference, PMID, DOI, NeuroSynthID, Experiments, Metadata" \
+                        " from Articles WHERE match (Title) against ('%{0}%' IN NATURAL LANGUAGE MODE)".format(text)
+        cur.execute(search_string)
+        for UniqueID,TIMESTAMP,Title,Authors,Abstract,Reference,PMID,DOI,NeuroSynthID,Experiments,Metadata in cur.fetchall():
+            database_dict = {}
+            database_dict["UniqueID"] = UniqueID
+            database_dict["TIMESTAMP"] = TIMESTAMP
+            database_dict["Title"] = Title
+            database_dict["Authors"] = Authors
+            database_dict["Abstract"] = Abstract
+            database_dict["Reference"] = Reference
+            database_dict["PMID"] = PMID
+            database_dict["DOI"] = DOI
+            database_dict["NeuroSynthID"] = NeuroSynthID
+            database_dict["Experiments"] = Experiments
+            database_dict["Metadata"] = Metadata
+        self.write(json.dumps(database_dict, sort_keys = True, indent = 4, separators = (',', ': ')))
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/go", StoryHandler),
+        (r"/about", AllExtras)
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    http_server = tornado.httpserver.HTTPServer(app)
+    port = int(os.environ.get("PORT", 5000))
+    http_server.listen(port) # hosts on localhost:5000
+    tornado.ioloop.IOLoop.current().start()
+

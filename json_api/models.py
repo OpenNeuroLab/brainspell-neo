1ef09ea18399460c430a60b18128dbd7d837a1ba
Neel Somani <neel@berkeley.edu>
2016-12-07 19:15:43 -0800

    instead of searching the entire abstract for each article, now we just search the title, authors, and metadata

--- json_api/models.py
+++ json_api/models.py
@@ -112,7 +112,7 @@ def create_tables(retry=5):
 
 def article_search(query, start):
     search = Articles.select(Articles.pmid, Articles.title, Articles.authors).where(
-        Match(Articles.title, query) | Match(Articles.title, query) | Match(Articles.abstract, query)
+        Match(Articles.title, query) | Match(Articles.authors, query) | Match(Articles.metadata, query)
     ).limit(10).offset(start)
     # return (search.count(), search.limit(10).offset(start).execute()) # give the total number of results, and output ten results, offset by "start"
     return search.execute() # search.count() makes the above line slow; TODO: find a better way of doing this

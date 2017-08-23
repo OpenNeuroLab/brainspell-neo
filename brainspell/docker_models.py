c6e7bb8eecc6f910974eb8d2d59b76b14d83bfbd
Sharabesh Ramesh <sharabesh@berkeley.edu>
2017-08-23 15:02:55 -0700

    Updating dockerfiles to reflect linked indepdendent containers

---
+++ brainspell/docker_models.py
@@ -0,0 +1,33 @@
+import os
+import time
+from urllib.parse import urlparse
+
+import peewee
+import playhouse
+import psycopg2
+from peewee import CharField, DateTimeField, IntegerField
+from playhouse import signals
+from playhouse.postgres_ext import *
+from playhouse.csv_loader import *
+
+
+
+config = dict(
+    database="docker",
+    user="docker",
+    password='docker',
+    host="0.0.0.0",
+    port=5432,
+    sslmode='require',
+)
+
+conn = PostgresqlExtDatabase(
+    autocommit=True,
+    autorollback=True,
+    register_hstore=False,
+    **config
+)
+
+
+load_csv(conn,'../docker/postgres/data/articles_2016_07-23.csv')
+

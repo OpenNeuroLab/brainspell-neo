55f898d3224406cb943919f721b43d3bf39ae77d
Neel Somani <neel@berkeley.edu>
2017-09-09 20:46:41 -0700

    disabling sauce connect until it works

--- setup-server.sh
+++ setup-server.sh
@@ -4,4 +4,11 @@
 
 pip2 install supervisor
 
-python3 server/generate_supervisor.py `which python3` `pwd` > server/supervisord.conf
+current_directory=${PWD##*/}
+
+if [ $current_directory = "brainspell-neo" ]
+then
+    python3 server/generate_supervisor.py `which python3` `pwd` > server/supervisord.conf
+else
+    echo "Not in the brainspell-neo directory. Please run again when you're in the correct directory."
+fi
